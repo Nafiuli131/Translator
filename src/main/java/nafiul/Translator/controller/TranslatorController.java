dd265c1a4597bbf68d55e57662ec36dab9de9527
Nafiul Islam <nafiul.islam@synesisit.info>
2025-04-06 14:22:17 +0600

    test api

--- src/main/java/nafiul/Translator/controller/TranslatorController.java
+++ src/main/java/nafiul/Translator/controller/TranslatorController.java
@@ -3,7 +3,9 @@ package nafiul.Translator.controller;
 import nafiul.Translator.dto.TranslatorDto;
 import nafiul.Translator.service.TranslatorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +19,10 @@ public class TranslatorController {
     @PostMapping("/getTranslator")
     public ResponseEntity<?> getTranslator(@RequestBody TranslatorDto translatorDto) {
         return translatorService.getTranslator(translatorDto);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<?> testApi(){
+        return new ResponseEntity<>("Application is deployed", HttpStatus.OK);
     }
 }
